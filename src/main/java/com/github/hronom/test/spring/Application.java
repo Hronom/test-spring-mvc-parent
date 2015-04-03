893bde36f528daf9b9d7d55aa2b3e0f21e6cd6c1
Hronom <hronom@gmail.com>
2015-04-03 20:32:12 +0300

    Initial commit

---
+++ src/main/java/com/github/hronom/test/spring/Application.java
@@ -0,0 +1,16 @@
+package com.github.hronom.test.spring;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    private static final Logger logger = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        logger.info("Application.main");
+        SpringApplication.run(new Class[] {Application.class}, args);
+    }
+}
