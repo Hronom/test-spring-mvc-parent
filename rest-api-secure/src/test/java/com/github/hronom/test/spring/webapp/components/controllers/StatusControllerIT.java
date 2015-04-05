bc3a3e9b2b6a9a896cd22f0ac614b52a3bd8589b
Hronom <hronom@gmail.com>
2015-04-05 22:16:35 +0300

    Add test for spring security

--- rest-api-secure/src/test/java/com/github/hronom/test/spring/webapp/components/controllers/StatusControllerIT.java
+++ rest-api-secure/src/test/java/com/github/hronom/test/spring/webapp/components/controllers/StatusControllerIT.java
@@ -1,6 +1,7 @@
 package com.github.hronom.test.spring.webapp.components.controllers;
 
 import com.github.hronom.test.spring.webapp.components.configurations.WebAppConfig;
+import com.github.hronom.test.spring.webapp.components.configurations.WebSecurityConfig;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,6 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -21,7 +23,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration(classes = {WebAppConfig.class}, loader = AnnotationConfigWebContextLoader.class)
+@ContextConfiguration(classes = {
+    WebSecurityConfig.class, WebAppConfig.class
+}, loader = AnnotationConfigWebContextLoader.class)
 public class StatusControllerIT {
     private MockMvc mockMvc;
 
@@ -35,13 +39,23 @@ public class StatusControllerIT {
 
     @Test
     public void testV1() throws Exception {
-        mockMvc.perform(get("/v1/status")).andDo(print()).andExpect(status().isOk())
-            .andExpect(content().json("{\"status\":true}"));
+        mockMvc.perform(
+            get("/v1/status").with(
+                user("user").password("user").roles("USER")
+            )
+        ).andDo(
+            print()
+        ).andExpect(
+            status().isOk()
+        ).andExpect(
+            content().json("{\"status\":true}")
+        );
     }
 
     @Test
     public void testV2() throws Exception {
-        mockMvc.perform(get("/v2/status")).andDo(print()).andExpect(status().isOk())
+        mockMvc.perform(get("/v2/status").with(user("admin").password("admin").roles("ADMIN")))
+            .andDo(print()).andExpect(status().isOk())
             .andExpect(content().json("{\"status\":true}"));
     }
 }
